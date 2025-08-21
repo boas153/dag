47acfc6dce25ff5e93847d9db11386ec8479ecfd
YoungHo Kim <younghokim@YoungHos-MacBook-Pro.local>
2025-08-21 22:19:37 +0900

    update

--- test_dags.py
+++ test_dags.py
@@ -1,10 +1,15 @@
-dags:
-  persistence:
-    enabled: true
-    existingClaim: ""
-    size: 1Gi
-    accessMode: ReadWriteMany
-  hostPath:
-    enabled: true
-    path: /Users/youngho/dev/airflow-dags
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+with DAG(
+    dag_id="test_dag",
+    start_date=datetime(2023, 1, 1),
+    schedule="@once",
+    catchup=False,
+) as dag:
+    hello = BashOperator(
+        task_id="hello_task",
+        bash_command="echo 'Hello Airflow from Macbook!'"
+    )
 
